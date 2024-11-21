abc2c2588907eaa6e75aada9f9728447f00d07c1
temidayo-code <your.new.email@example.com>
2024-11-21 10:56:54 +0100

    updating the backend URL

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Middleware
 app.use(cors());
